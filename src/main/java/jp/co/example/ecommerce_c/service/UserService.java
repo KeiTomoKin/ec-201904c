4ccad6b4ad776abeec9789cc5eba96cbb9401c08
takara.miyazaki <takara.miyazaki@rakus.co.jp>
2019-06-19 15:38:34 +0900

    Userのレポジトリとサービスを作成

---
+++ src/main/java/jp/co/example/ecommerce_c/service/UserService.java
@@ -0,0 +1,47 @@
+package jp.co.example.ecommerce_c.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.example.ecommerce_c.domain.User;
+import jp.co.example.ecommerce_c.repository.UserRepository;
+
+/**
+ * ユーザー情報を操作するサービス.
+ * 
+ * @author takara.miyazaki
+ *
+ */
+@Service
+@Transactional
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	/**
+	 * ユーザ情報を登録します.
+	 * 
+	 * @param administrator ユーザ情報
+	 */
+	public void insert(User user) {
+		// TODO
+	}
+
+	/**
+	 * ログインする.
+	 * 
+	 * @param email    メールアドレス
+	 * @param password パスワード
+	 * @return ユーザ情報 存在しない場合はnullが返る
+	 */
+	public User login(String email, String password) {
+		User user = userRepository.findByMailAddressAndPassward(email, password);
+		if (user == null) {
+			return null;
+		}
+		return user;
+	}
+
+}
