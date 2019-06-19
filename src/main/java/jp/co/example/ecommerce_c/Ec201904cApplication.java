ec4c08c3127439d3df90dafef7fa2dde320b7b56
KAZUYA215074 <nrtgntm@me.com>
2019-06-19 10:38:00 +0900

    最初のコミット

---
+++ src/main/java/jp/co/example/ecommerce_c/Ec201904cApplication.java
@@ -0,0 +1,13 @@
+package jp.co.example.ecommerce_c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec201904cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec201904cApplication.class, args);
+	}
+
+}
