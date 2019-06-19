21104ed3540aa404251cdd6e67d2513028e7beb9
KAZUYA215074 <nrtgntm@me.com>
2019-06-19 11:41:54 +0900

    ドメインのTopping、OrderToppingクラスを作成

---
+++ src/main/java/jp/co/example/ecommerce_c/domain/OrderTopping.java
@@ -0,0 +1,56 @@
+package jp.co.example.ecommerce_c.domain;
+
+/**
+ * 注文トッピングのドメイン
+ * 
+ * @author kazuya.makida
+ *
+ */
+public class OrderTopping {
+	//** ID **/
+	private Integer id;
+	//** トッピング **/
+	private Integer toppingId;
+	//** 注文商品のID **/
+	private Integer orderItemId;
+	//** トッピング **/
+	private Topping topping;
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", toppingId=" + toppingId + ", orderItemId=" + orderItemId + ", topping=" + topping + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getToppingId() {
+		return toppingId;
+	}
+
+	public void setToppingId(Integer toppingId) {
+		this.toppingId = toppingId;
+	}
+
+	public Integer getOrderItemId() {
+		return orderItemId;
+	}
+
+	public void setOrderItemId(Integer orderItemId) {
+		this.orderItemId = orderItemId;
+	}
+
+	public Topping getTopping() {
+		return topping;
+	}
+
+	public void setTopping(Topping topping) {
+		this.topping = topping;
+	}
+
+}
