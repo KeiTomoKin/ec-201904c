6d9aafd39e90df373a86b61d32985420f5645bb4
tomookakeita <keita516tomo@outlook.jp>
2019-06-19 15:10:08 +0900

    リポジトリのItemクラスのマッパーを作成

---
+++ src/main/java/jp/co/example/ecommerce_c/repository/ItemRepository.java
@@ -0,0 +1,19 @@
+package jp.co.example.ecommerce_c.repository;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import jp.co.example.ecommerce_c.domain.Item;
+
+public class ItemRepository {
+	private RowMapper<Item> ITEM_ROW_MAPPER = (rs, i) -> {
+		Item item = new Item();
+		item.setId(rs.getInt("id"));
+		item.setName(rs.getString("name"));
+		item.setDescription(rs.getString("description"));
+		item.setPriceM(rs.getInt("price_m"));
+		item.setPriceL(rs.getInt("price_l"));
+		item.setImagePath(rs.getString("image_path"));
+		item.setDeleted(rs.getBoolean("deleted"));
+		return item;
+	};
+}
