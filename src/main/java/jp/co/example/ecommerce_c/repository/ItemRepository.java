1763ab1b6870ad9948bcb25c3272e775fedbcde0
tomookakeita <keita516tomo@outlook.jp>
2019-06-19 15:39:05 +0900

    リポジトリのItemクラスに商品情報を取得する機能を追加

--- src/main/java/jp/co/example/ecommerce_c/repository/ItemRepository.java
+++ src/main/java/jp/co/example/ecommerce_c/repository/ItemRepository.java
@@ -1,11 +1,24 @@
 package jp.co.example.ecommerce_c.repository;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import jp.co.example.ecommerce_c.domain.Item;
 
+/**
+ * Itemsテーブルを操作するリポジトリ
+ * 
+ * @author keita.tomooka
+ * @author takuya.aramaki
+ *
+ */
+@Repository
 public class ItemRepository {
-	private RowMapper<Item> ITEM_ROW_MAPPER = (rs, i) -> {
+	private static final RowMapper<Item> ITEM_ROW_MAPPER = (rs, i) -> {
 		Item item = new Item();
 		item.setId(rs.getInt("id"));
 		item.setName(rs.getString("name"));
@@ -16,4 +29,19 @@ public class ItemRepository {
 		item.setDeleted(rs.getBoolean("deleted"));
 		return item;
 	};
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	/**
+	 * 商品情報を全て取得する.
+	 * 
+	 * @return 商品情報のリスト
+	 */
+	public List<Item> findAll() {
+		String sql = "SELECT id, name, description, price_m, price_l, image_path, deleted FROM items ";
+		List<Item> itemList = template.query(sql, ITEM_ROW_MAPPER);
+		return itemList;
+	}
+
 }
