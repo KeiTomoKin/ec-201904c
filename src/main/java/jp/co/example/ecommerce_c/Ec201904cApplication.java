23aefc5711c14edb6898ee2cc81c384f23afd8a8
Takuya Aramaki <takuya.aramaki@rakus.co.jp>
2019-06-24 14:35:08 +0900

    メール送信時、画面表示を送信完了まで待たないように修正

--- src/main/java/jp/co/example/ecommerce_c/Ec201904cApplication.java
+++ src/main/java/jp/co/example/ecommerce_c/Ec201904cApplication.java
@@ -2,8 +2,10 @@ package jp.co.example.ecommerce_c;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Ec201904cApplication {
 
 	public static void main(String[] args) {
