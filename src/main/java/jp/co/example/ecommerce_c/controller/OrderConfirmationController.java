41abbe02062bf624b7ff8b2cb89f07478a8b2317
Takuya Aramaki <takuya.aramaki@rakus.co.jp>
2019-06-25 14:53:26 +0900

    注文画面で登録済みの個人情報が入力されるようにしました

--- src/main/java/jp/co/example/ecommerce_c/controller/OrderConfirmationController.java
+++ src/main/java/jp/co/example/ecommerce_c/controller/OrderConfirmationController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,7 +12,9 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.example.ecommerce_c.domain.LoginUser;
 import jp.co.example.ecommerce_c.domain.Order;
+import jp.co.example.ecommerce_c.domain.User;
 import jp.co.example.ecommerce_c.form.OrderConfirmationForm;
 import jp.co.example.ecommerce_c.service.OrderConfirmationService;
 
@@ -31,8 +34,22 @@ public class OrderConfirmationController {
 	private HttpSession session;
 
 	@ModelAttribute
-	public OrderConfirmationForm setOrderCompleteForm() {
-		return new OrderConfirmationForm();
+	public OrderConfirmationForm setOrderCompleteForm(@AuthenticationPrincipal LoginUser loginUser) {
+		OrderConfirmationForm orderConfirmationForm = new OrderConfirmationForm();
+		User user = loginUser.getUser();
+		String[] userName = user.getName().split(" ");
+		String[] userTel = user.getTelephone().split("-");
+
+		orderConfirmationForm.setDestinationFirstName(userName[0]);
+		orderConfirmationForm.setDestinationLastName(userName[1]);
+		orderConfirmationForm.setDestinationEmail(user.getEmail());
+		orderConfirmationForm.setDestinationZipcode(user.getZipcode());
+		orderConfirmationForm.setDestinationAddress(user.getAddress());
+		orderConfirmationForm.setDestinationFirstTel(userTel[0]);
+		orderConfirmationForm.setDestinationMiddleFirstTel(userTel[1]);
+		orderConfirmationForm.setDestinationLastFirstTel(userTel[2]);
+
+		return orderConfirmationForm;
 	}
 
 	/**
@@ -43,18 +60,18 @@ public class OrderConfirmationController {
 	 */
 	@RequestMapping("")
 	public String confirmOrder(Model model) {
-//		Integer orderId = (Integer) session.getAttribute("orderId");
-//		Order order = orderConfirmationService.getOrder(orderId);
-//		model.addAttribute("order", order);
+		Integer orderId = (Integer) session.getAttribute("orderId");
+		Order order = orderConfirmationService.getOrder(orderId);
+		model.addAttribute("order", order);
 
 		return "order_confirm";
 	}
 
 	@RequestMapping("/orderComplete")
-	public String orderComplete(@Validated OrderConfirmationForm form, BindingResult result) {
+	public String orderComplete(@Validated OrderConfirmationForm form, BindingResult result, Model model) {
 		// 入力値チェック
 		if (result.hasErrors()) {
-			return "order_confirm";
+			return confirmOrder(model);
 		}
 
 		// 注文情報の作成
