fb1321ab80a11af524a037baab57b2ed8048b4e1
伊賀将之 <igamasayuki@igamasayukinoMacBook-puro.local>
2019-06-26 11:56:54 +0900

    jUnitテストが失敗してしまう現象を解消しました

--- src/test/java/com/example/Ec201904cApplicationTests.java
+++ src/test/java/com/example/Ec201904cApplicationTests.java
@@ -3,10 +3,14 @@ package com.example;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import jp.co.example.ecommerce_c.Ec201904cApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes = Ec201904cApplication.class)
 public class Ec201904cApplicationTests {
 
 	@Test
